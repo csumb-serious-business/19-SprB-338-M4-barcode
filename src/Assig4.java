7a1faa79d63e328ec1e4c1f28fba3c171dc002f2
Chatura Ahangama <chaturadroid@gmail.com>
2019-03-31 18:20:04 -0700

    updates with the cleaning implemented

--- src/Assig4.java
+++ src/Assig4.java
@@ -22,32 +22,53 @@ interface BarcodeIO {
 
 public class Assig4 {
 	public static void main(String[] args) {
-		String[] sImageIn = { "                                               ",
-				"                                               ", "                                               ",
-				"     * * * * * * * * * * * * * * * * * * * * * ", "     *                                       * ",
-				"     ****** **** ****** ******* ** *** *****   ", "     *     *    ****************************** ",
-				"     * **    * *        **  *    * * *   *     ", "     *   *    *  *****    *   * *   *  **  *** ",
-				"     *  **     * *** **   **  *    **  ***  *  ", "     ***  * **   **  *   ****    *  *  ** * ** ",
-				"     *****  ***  *  * *   ** ** **  *   * *    ", "     ***************************************** ",
-				"                                               ", "                                               ",
-				"                                               "
+		  String[] sImageIn =
+		      {
+		         "                                               ",
+		         "                                               ",
+		         "                                               ",
+		         "     * * * * * * * * * * * * * * * * * * * * * ",
+		         "     *                                       * ",
+		         "     ****** **** ****** ******* ** *** *****   ",
+		         "     *     *    ****************************** ",
+		         "     * **    * *        **  *    * * *   *     ",
+		         "     *   *    *  *****    *   * *   *  **  *** ",
+		         "     *  **     * *** **   **  *    **  ***  *  ",
+		         "     ***  * **   **  *   ****    *  *  ** * ** ",
+		         "     *****  ***  *  * *   ** ** **  *   * *    ",
+		         "     ***************************************** ",  
+		         "                                               ",
+		         "                                               ",
+		         "                                               "
 
-		};
+		      };      
+		            
+		         
+		      
+		      String[] sImageIn_2 =
+		      {
+		            "                                          ",
+		            "                                          ",
+		            "* * * * * * * * * * * * * * * * * * *     ",
+		            "*                                    *    ",
+		            "**** *** **   ***** ****   *********      ",
+		            "* ************ ************ **********    ",
+		            "** *      *    *  * * *         * *       ",
+		            "***   *  *           * **    *      **    ",
+		            "* ** * *  *   * * * **  *   ***   ***     ",
+		            "* *           **    *****  *   **   **    ",
+		            "****  *  * *  * **  ** *   ** *  * *      ",
+		            "**************************************    ",
+		            "                                          ",
+		            "                                          ",
+		            "                                          ",
+		            "                                          "
 
-		String[] sImageIn_2 = { "                                          ",
-				"                                          ", "* * * * * * * * * * * * * * * * * * *     ",
-				"*                                    *    ", "**** *** **   ***** ****   *********      ",
-				"* ************ ************ **********    ", "** *      *    *  * * *         * *       ",
-				"***   *  *           * **    *      **    ", "* ** * *  *   * * * **  *   ***   ***     ",
-				"* *           **    *****  *   **   **    ", "****  *  * *  * **  ** *   ** *  * *      ",
-				"**************************************    ", "                                          ",
-				"                                          ", "                                          ",
-				"                                          "
-
-		};
+		      };
 
 		System.out.print("Start");
 		BarcodeImage bc = new BarcodeImage(sImageIn);
+		bc.display();
 		DataMatrix dm = new DataMatrix(bc);
 
 		// First secret message
@@ -79,17 +100,18 @@ class BarcodeImage implements Cloneable {
 		this();
 
 		// Todo Check Size
-
-		for (int i = 0; i < strIn.length; i++) // How many row
+		for (int iRow = 0; iRow < strIn.length; iRow++) //For each item in the array
 		{
-			for (int j = 0; j < strIn[0].length(); j++) // How many in row
+			int numOfColums = strIn[0].length();
+			
+			for (int iColumn = 0; iColumn < numOfColums; iColumn++) // How many in each item in the array (row items)
 			{
-				char[] raw = strIn[i].toCharArray();
-				int rowIndex = MAX_HEIGHT - (strIn.length - i);
-				if (raw[j] == '*') {
-					setPixel(rowIndex, j, true);
+				char[] raw = strIn[iRow].toCharArray();
+				int rowIndex = MAX_HEIGHT - (strIn.length - iRow); //This way we cut down on the extra spacing 
+				if (raw[iColumn] == '*') {
+					setPixel(rowIndex, iColumn, true);
 				} else {
-					setPixel(rowIndex, j, false);
+					setPixel(rowIndex, iColumn, false);
 				}
 			}
 		}
@@ -115,15 +137,16 @@ class BarcodeImage implements Cloneable {
 		return imageData[row][col];
 	}
 
+	@Override
 	public BarcodeImage clone() throws CloneNotSupportedException {
 		BarcodeImage returnBC = null;
 		try {
 			returnBC = (BarcodeImage) super.clone();
 
 			// Deep Copy
-			for (int i = 0; i < MAX_HEIGHT; i++) {
-				for (int j = 0; j < MAX_WIDTH; j++) {
-					returnBC.imageData[i][j] = this.getPixel(i, j);
+			for (int iRow = 0; iRow < MAX_HEIGHT; iRow++) {
+				for (int iCol = 0; iCol < MAX_WIDTH; iCol++) {
+					returnBC.imageData[iRow][iCol] = this.getPixel(iRow, iCol);
 				}
 			}
 
@@ -135,7 +158,27 @@ class BarcodeImage implements Cloneable {
 		return returnBC;
 
 	}
+	
+	public void display() {
+		      int iRow, iCol;
+		      System.out.println();
+		      for(iRow = 0; iRow < MAX_HEIGHT; iRow++)
+		      {
+		         System.out.print("|"+iRow+"|");
+		         for(iCol = 0; iCol < MAX_WIDTH; iCol++)
+		         {
+		            if(getPixel(iRow, iCol) == true) 
+		            {
+		               System.out.print("*");
+		            } else {
+		               System.out.print(" ");
+		            }
+		         }
+		         System.out.println();
+		      }
+		   }
 
+		
 }
 
 class DataMatrix implements BarcodeIO {
@@ -146,6 +189,7 @@ class DataMatrix implements BarcodeIO {
 	private int actualWidth, actualHeight = 0;
 	private String text;
 	private static final String defText = "undefined";
+	
 
 	public DataMatrix() {
 		image = new BarcodeImage();
@@ -178,9 +222,18 @@ class DataMatrix implements BarcodeIO {
 
 	}
 
-	public boolean scan(BarcodeImage _) {
-
-		return false;
+	public boolean scan(BarcodeImage imageIN) {
+		 try 
+	      {
+	         this.image = (BarcodeImage) imageIN.clone();
+	         cleanImage(); //Adjusting to the left bottom
+	         actualHeight = computeSignalHeight();
+	 	     actualWidth = computeSignalWidth();
+	      } catch(CloneNotSupportedException e)
+	      {
+	       
+	      }
+	      return true;
 	}
 
 	public boolean readText(String _) {
@@ -191,9 +244,85 @@ class DataMatrix implements BarcodeIO {
 		return false;
 	}
 
-	// Private Helpers For image manipulation
+	// Private Helpers For image manipulation to move it to the left bottom
 	private void cleanImage() {
+		   //Temp Holders to get the Starting point of the image before the optimiztion
+		   int rowStart=0, colStart = 0;
+		   
+		   outter:
+		   for (int indexHorizontal = BarcodeImage.MAX_HEIGHT-1; indexHorizontal >= 0; indexHorizontal--) 
+		   {
+		
+			   for (int indexVertical = 0; indexVertical < BarcodeImage.MAX_WIDTH; indexVertical++) {
+				   if (image.getPixel(indexHorizontal, indexVertical)) //Pixel detected
+				   {
+					   if(rowStart == 0 && colStart == 0)//Check if the we are at the start 
+					   {
+						 //Hold the value of where the first pixel is detected
+						   rowStart = indexHorizontal; 
+						   colStart = indexVertical;
+						   break  outter;//If detected break out of the outter loop
+					   }
+				   }
+			   }
+		   }
+		   
+	
+		   for (int indexVeriticle = 0; indexVeriticle < BarcodeImage.MAX_HEIGHT; indexVeriticle++) {
+			   for (int indexHorizontal = 0; indexHorizontal < BarcodeImage.MAX_WIDTH; indexHorizontal++) {
+				   if(rowStart - indexVeriticle >= 0) //Can not be a negative 
+				   {
+					   if( colStart + indexHorizontal < BarcodeImage.MAX_WIDTH) // Has to be less that the width 
+					   {
+						   //Mapping the old to new position
+						   int newRow = BarcodeImage.MAX_HEIGHT-indexVeriticle-1; //Start at the top
+						   int newCol = indexHorizontal;
+						   
+						   int oldRow = rowStart-indexVeriticle; //Go Up
+						   int oldCol = colStart+indexHorizontal; //Go Right
+						   
+						   image.setPixel(newRow , newCol, image.getPixel(oldRow, oldCol));
+					   }
+				   }
+			   }
+		   }
+		    image.display(); //debugging
+	   }
+	
+	//Todo: rough idea
+	 int computeSignalWidth() {
+	      int leftCornerRow = image.MAX_HEIGHT-1;
+	      int widthCount = 0;
+	      for(int indexColumn=0; indexColumn<image.MAX_WIDTH; indexColumn++) 
+	      { //Keep going right
+	         if(image.getPixel(leftCornerRow, indexColumn)) 
+	         {
+	            widthCount++;
+	         }
+	      }	      
+		 return widthCount;
+	 }
 
-	}
+	 //Todo: rough idea
+	 int computeSignalHeight() {
+	      int leftCornerCol = 0;
+	      int heightCount = 0;
+	      for(int indexRow=0; indexRow<image.MAX_HEIGHT; indexRow++) 
+	      { //Keep going up
+	         if(image.getPixel(indexRow,  leftCornerCol)) 
+	         {
+	        	 heightCount ++;
+	         }
+	      }	      
+		 return  heightCount;
+	 }
+
+	 private char readCharFromCol(int col) {
+		 return 'a';
+	 }
+
+	 private boolean WriteCharToCol(int col, int code){
+		 return true;
+	 }
 
 }
